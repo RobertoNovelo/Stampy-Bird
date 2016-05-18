3d43494e29c2e12ee672c47855694e62873b75d5
Roberto Novelo <robertod.novelo@gmail.com>
2016-05-18 13:33:57 -0500

    Envelope Pipes, Bug Fixing

--- src/js/components/graphics/pipe.js
+++ src/js/components/graphics/pipe.js
@@ -8,21 +8,11 @@ PipeGraphicsComponent.prototype.draw = function(context) {
     var position = this.entity.components.physics.position;
     var size = this.entity.size;
 
-    if(envelopeImageLoaded)
-    {
-		context.save();
-		context.translate(position.x, position.y);
-		context.rotate(Math.PI/2);
-		context.drawImage(this.envimage, 0,0, this.envimage.width,this.envimage.height, 0,0, size.y,-size.x);
-		context.restore();
-    }
-    else
-    {
-		context.save();
-		context.fillStyle = "green";
-		context.fillRect(position.x, position.y, size.x, size.y);
-		context.restore();
-	}
+	context.save();
+	context.translate(position.x, position.y);
+	context.rotate(Math.PI/2);
+	context.drawImage(this.envimage,0,0,size.y,-size.x);
+	context.restore();
 }
 
 exports.PipeGraphicsComponent = PipeGraphicsComponent;
